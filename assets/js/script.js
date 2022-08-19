202f060a39685f5c87088239f1f335f5469001fc
John <John.Arscott@hotmail.com>
2022-08-19 14:08:48 +0000

    first link

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const response = fetch("https://ci-jshint.herokuapp.com/api", {
+                        method: "POST",
+                        headers: {
+                                    "Authorization": 3dNa720kcPbrE46w-yvSCZE-fvQ,
+                                 }
+                        })
+
